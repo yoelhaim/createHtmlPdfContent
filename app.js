b910050b7eab26f3afb3e0ce48577b4940b30c72
yoelhaim <youssefelhaimer8@gmail.com>
2024-09-26 12:04:55 +0100

    chore: This project generates customized PDFs from HTML templates using Handlebars. It compiles dynamic data into HTML, embeds images, and creates a downloadable PDF with specified formatting, leveraging browser automation for accurate rendering.

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+const handlebars = require("handlebars");
+const scrap = require("./createHtmlPdfContent");
+const app = express();
+const port = 3099;
+// import path from "path";
+// import fs from "fs/promises";
+const path = require("path");
+const fs = require("fs/promises");
+
+const pdf = require("html-pdf");
+
+app.get("/", async (req, res) => {
+  const templateSource = `./file.html`;
+
+  const templateData = {
+    items: [
+      {
+        name: "Product 1",
+        quantity: 2,
+        price: 100,
+      },
+      {
+        name: "Product 2",
+        quantity: 1,
+        price: 200,
+      },
+    ],
+    shippingAmount: 100,
+    id: 1,
+    taxes: 10,
+    fullname: "hello everyone",
+  };
+
+  const { html, pdf, ...rest } = await scrap(templateData, templateSource);
+
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ createHtmlPdfContent.js
@@ -0,0 +1,89 @@
+const { Builder, By, until } = require("selenium-webdriver");
+const handlebars = require("handlebars");
+const path = require("path");
+const fs = require("fs/promises");
+const chrome = require("selenium-webdriver/chrome");
+const pdf = require("html-pdf");
+
+const generatePdfWithSelenium = async (templateData, fileSource) => {
+  try {
+    const data = templateData;
+    if (!fileSource) {
+      return { error: "Invalid file source" };
+    }
+    const resvfile = fileSource.split("").reverse().join("").split(".")[0];
+    if (resvfile.split("").reverse().join("") !== "html") {
+      return { error: "Invalid file format" };
+    }
+
+    const templatePath = path.join(process.cwd(), fileSource);
+
+    const templateSource = await fs.readFile(templatePath, "utf-8");
+
+    // Chrome Options (use headless if needed, here headless is set to false)
+    const options = new chrome.Options();
+    // options.setChromeBinaryPath(
+    //   "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+    // );
+    options.addArguments("--no-sandbox", "--disable-setuid-sandbox");
+
+    // Launch the browser with Selenium
+    let driver = await new Builder()
+      .forBrowser("chrome")
+      .setChromeOptions(options)
+      .build();
+
+    try {
+      const content = templateSource;
+
+      // Compile Handlebars template
+      const template = handlebars.compile(content);
+      const result = template(data);
+
+      // Convert logo to Base64
+      const logoPath = path.join(process.cwd(), "./logo.png");
+      const logoData = await fs.readFile(logoPath);
+      const logoBase64 = logoData.toString("base64");
+      const logoDataUrl = `data:image/png;base64,${logoBase64}`;
+
+      // Replace logo placeholder in HTML
+      const htmlContentWithDataUrl = result.replace(
+        'src="logo.png"',
+        `src="${logoDataUrl}"`
+      );
+
+      await driver.get(
+        "data:text/html;charset=utf-8," +
+          encodeURIComponent(htmlContentWithDataUrl)
+      );
+
+      await driver.wait(until.elementLocated(By.tagName("body")), 10000);
+
+      await driver.quit();
+      const genpdf = await pdf.create(htmlContentWithDataUrl, {
+        format: "A4",
+        printBackground: true,
+        margin: { top: "10mm", bottom: "10mm", left: "10mm", right: "10mm" },
+      });
+      const buff = await new Promise((resolve, reject) => {
+        genpdf.toBuffer((err, buff) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve(buff);
+        });
+      });
+
+      return { html: htmlContentWithDataUrl, pdf: buff };
+    } catch (error) {
+      console.error("Error in the browser interaction", error);
+      await driver.quit();
+      return { error };
+    }
+  } catch (error) {
+    console.error("Error generating PDF", error);
+    return { error };
+  }
+};
+
+module.exports = generatePdfWithSelenium;
